bf2d3fdea9cb1a613658ba5968ffdd85b44aa140
Bruno Rondon da Silva <bruno.engmeca@gmail.com>
2021-04-25 19:08:06 -0400

    Criando a arquitetura do projeto e definindo contratos.

---
+++ SOAP/Pacagroup.Comercial.Creditos.Contrato/IClienteService.cs
@@ -0,0 +1,16 @@
+﻿using Pacagroup.Comercial.Creditos.Dominio;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Pacagroup.Comercial.Creditos.Contrato
+{
+    [ServiceContract]
+    public interface IClienteService
+    {
+        [OperationContract]
+        Cliente ObtenerCliente(string numeroDocumento);
+        [OperationContract]
+        IEnumerable<Cliente> ListarCliente();
+
+    }
+}
---
+++ SOAP/Pacagroup.Comercial.Creditos.Implementacion/ClienteService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Pacagroup.Comercial.Creditos.Contrato;
+using Pacagroup.Comercial.Creditos.Dominio;
+
+namespace Pacagroup.Comercial.Creditos.Implementacion
+{
+    public class ClienteService : IClienteService
+    {
+        public IEnumerable<Cliente> ListarCliente()
+        {
+            return null;
+        }
+
+        public Cliente ObtenerCliente(string numeroDocumento)
+        {
+            return null;
+        }
+    }
+}
